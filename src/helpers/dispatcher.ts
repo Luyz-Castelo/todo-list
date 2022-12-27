566ebfcb94aca44e88cb66c1232466fb27bdde5a
lcbranco <luyzcastelobranco@gmail.com>
2022-12-27 15:14:53 -0300

    litte correction

--- src/helpers/dispatcher.ts
+++ src/helpers/dispatcher.ts
@@ -2,14 +2,14 @@ const createDispatcher = () => {
   const callbackLists: { [key: string]: Function[] } = {};
 
   const dispatch = (eventName: string, data: any) => {
-    let callbackList = callbackLists[eventName];
+    const callbackList = callbackLists[eventName];
     if (!callbackList) return;
 
     for (const callback of callbackList) {
       callback(data)
     }
   };
-  
+
   const listen = (eventName: string, callback: Function) => {
     if (!callbackLists[eventName]) {
       callbackLists[eventName] = []
